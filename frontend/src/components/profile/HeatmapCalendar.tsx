81cd354fd59d8d598363f3dbad5389307a211e1d
CoderNandan <nandanindia1947@gmail.com>
2025-05-30 01:01:05 +0530

    feat: enhance HeatmapCalendar styling and improve color scale definitions

--- frontend/src/components/profile/HeatmapCalendar.tsx
+++ frontend/src/components/profile/HeatmapCalendar.tsx
@@ -2,18 +2,18 @@ import CalendarHeatmap from "react-calendar-heatmap";
 import type { SubmissionHeatmapEntry } from "@/types";
 import "react-calendar-heatmap/dist/styles.css";
 import { Card, CardContent } from "../ui/card";
-
 interface HeatmapCalendarProps {
   data: SubmissionHeatmapEntry[];
   isLoading: boolean;
 }
+
 export default function HeatmapCalendar({
   data,
   isLoading,
 }: HeatmapCalendarProps) {
   console.log("data", data, isLoading);
   return (
-    <section className="mb-4 flex flex-col flex-wrap items-start pr-3">
+    <section className="mb-4 flex flex-col flex-wrap items-start">
       <div className="mb-2">
         <strong className="ml-3">Heatmap ↓</strong>
       </div>
@@ -24,6 +24,12 @@ export default function HeatmapCalendar({
             startDate={new Date("2025-01-01")}
             endDate={new Date("2025-12-31")}
             values={data}
+            classForValue={(value) => {
+              if (!value || value.count === 0) return "color-empty";
+              if (value.count >= 5) return "color-github-5";
+              return `color-github-${value.count}`;
+            }}
+            showWeekdayLabels={false}
           />
         </CardContent>
       </Card>
