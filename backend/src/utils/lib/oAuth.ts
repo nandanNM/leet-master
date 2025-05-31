c802c779e13291eabbf967e2e9e49aecafa89436
CoderNandan <nandanindia1947@gmail.com>
2025-05-31 13:56:04 +0530

    add passport js

---
+++ backend/src/utils/lib/oAuth.ts
@@ -0,0 +1,120 @@
+import passport from "passport";
+import {
+  Strategy as GoogleStrategy,
+  VerifyCallback,
+} from "passport-google-oauth20";
+import "dotenv/config";
+import {Request} from "express";
+import {eq} from "drizzle-orm";
+import {usersTable} from "src/db/schema";
+import {db} from "src/db";
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL!,
+      scope: ["profile", "email"],
+      passReqToCallback: true,
+    },
+    async (
+      req: Request,
+      accessToken: string,
+      refreshToken: string,
+      profile,
+      done: VerifyCallback,
+    ) => {
+      try {
+        const email = profile?.emails?.[0]?.value;
+        const displayName = profile.displayName || "";
+        const photo = profile.photos?.[0]?.value;
+        console.log(req, accessToken, refreshToken);
+        if (!email) {
+          throw new Error("Email not provided by Google");
+        }
+
+        const user = await db.query.usersTable.findFirst({
+          where: eq(usersTable.email, email),
+          columns: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true,
+            bio: true,
+            role: true,
+          },
+        });
+
+        if (user) {
+          if (!user.avatar && photo) {
+            const [updateUser] = await db
+              .update(usersTable)
+              .set({
+                avatar: photo,
+              })
+              .where(eq(usersTable.id, user.id))
+              .returning({
+                id: usersTable.id,
+                email: usersTable.email,
+                name: usersTable.name,
+                avatar: usersTable.avatar,
+                bio: usersTable.bio,
+                role: usersTable.role,
+              });
+            if (!updateUser) return done(null, false);
+            return done(null, updateUser);
+          }
+        }
+        const [newUser] = await db
+          .insert(usersTable)
+          .values({
+            name: displayName,
+            email,
+            avatar: photo,
+            authProvider: "google",
+          })
+          .returning({
+            id: usersTable.id,
+            email: usersTable.email,
+            name: usersTable.name,
+            avatar: usersTable.avatar,
+            bio: usersTable.bio,
+            role: usersTable.role,
+          });
+        done(null, newUser);
+      } catch (error) {
+        done(
+          error instanceof Error ? error : new Error("Authentication failed"),
+        );
+      }
+    },
+  ),
+);
+
+// Serialization for session
+passport.serializeUser((user: any, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id: string, done) => {
+  try {
+    const user = await db.query.usersTable.findFirst({
+      where: eq(usersTable.id, id),
+      columns: {
+        id: true,
+        name: true,
+        email: true,
+        avatar: true,
+        bio: true,
+        role: true,
+      },
+    });
+
+    if (!user) return done(null, false);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+export default passport;
